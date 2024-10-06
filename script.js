07b1173b9dfc459fb12bdd3b05951901fa9ed543
AnastasiYA-01 <eas-06@yandex.ru>
2024-10-06 23:32:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputEl = (document.getElementsByClassName('app__controls-input'))[0]
+const btnEl = (document.getElementsByClassName('app__controls-button'))[0]
+const listEl = (document.getElementsByClassName('app__list'))[0]
+
+//console.log(inputEl);
+//console.log(btnEl);
+//console.log(listEl);
+
+
+function createTask(someText) {
+    const root = document.createElement('div')
+    root.classList.add('app_list-item')
+
+    const input = document.createElement('input')
+    input.classList.add('app__list-checkbox')
+    input.type = 'checkbox'
+
+    const txt = document.createElement('p')
+    txt.classList.add('app_list-item-text')
+    txt.innerText = someText
+
+    const btn = document.createElement('button')
+    btn.classList.add('app__list-btn')
+
+    const img = document.createElement('img')
+    img.src = './12.png'
+    img.alt = 'trash'
+
+    btn.appendChild(img)
+
+    root.appendChild(input)
+    root.appendChild(txt)
+    root.appendChild(btn)
+}
+
+btnEl.addEventListener('click', () => {
+    const textValue = inputEl.value
+    console.log(textValue);
+    
+})
